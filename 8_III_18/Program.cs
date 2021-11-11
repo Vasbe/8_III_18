824fc098ec74949ec9d10d5a109358722084c00a
Vasbe <vasbeme@gmail.com>
2021-11-11 14:12:12 +0400

    Добавьте файлы проекта.

---
+++ 8_III_18/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+namespace _8_III_18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введитие строку: ");
+            string s = Console.ReadLine();
+            string[] writes = s.Split();
+
+            Console.Write("Введите число повторений: ");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < writes.Length; i++)
+            {
+                string write = writes[i];
+
+                int count = 0;
+                foreach (string words in writes)
+                {
+                    if (words == write)
+                        count++;
+                }
+                if (count >= n) {
+                        Console.WriteLine(writes[i]);
+                }
+                    
+            }
+        }
+    }
+}
